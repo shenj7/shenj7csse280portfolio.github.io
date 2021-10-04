2c9c168e455f366531ed98a62b6afbe40d5d5815
Shen <shenj7@rose-hulman.edu>
2021-10-03 22:59:55 -0400

    finished

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,36 +1,14 @@
-/**
- * @fileoverview
- * Provides the JavaScript interactions for all pages.
- *
- * @author 
- * PUT_YOUR_NAME_HERE
- */
-
-/** namespace. */
 var rhit = rhit || {};
 
-/** globals */
-rhit.variableName = "";
-
-/** function and class syntax examples */
-rhit.functionName = function () {
-	/** function body */
-};
-
-rhit.ClassName = class {
-	constructor() {
-
-	}
-
-	methodName() {
-
-	}
-}
-
-/* Main */
-/** function and class syntax examples */
 rhit.main = function () {
-	console.log("Ready");
+    let isjp = false;
+    $('[lang="jp"]').hide();
+    $('#change-lang').click(function() {
+        $('[lang="en"]').toggle();
+        $('[lang="jp"]').toggle();
+        isjp = !isjp;
+        $('#change-lang').html(isjp?"English":"日本語");
+      });
 };
 
 rhit.main();
